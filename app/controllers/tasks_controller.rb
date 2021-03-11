171817b7b462fee446e39a9127aeb04b5f1f2d9c
jessica.farias.rosado@gmail.com <jessica.farias.rosado@gmail.com>
2021-03-10 18:05:15 -0600

    docs: Adding create and destroy for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,26 @@ class TasksController < ApplicationController
     @tasks = Task.all
     render json: @tasks
   end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+        render json: @task, status: :created, location: @task
+    else
+        render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    @task = Book.find(params[:id])
+    @task.destroy
+  end
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :goal, :expiration_day, :user_id)
+  end
 end
