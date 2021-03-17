b9159eaa16c242f1e8cefc81c5dcc5a7c835128a
jessica.farias.rosado@gmail.com <jessica.farias.rosado@gmail.com>
2021-03-16 23:17:41 -0600

    docs: Change response message to create tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,7 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.new(task_params)
       if @task.save
-        render json: @task, message: task_params
+        render_success(message: 'OK')
       else
         render json: @task.errors, status: :unprocessable_entity
       end
