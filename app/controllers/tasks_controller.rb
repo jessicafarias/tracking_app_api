22826dfb16d843c6d66942b186419c7efe6bf014
jessica.farias.rosado@gmail.com <jessica.farias.rosado@gmail.com>
2021-03-18 00:48:24 -0600

    docs: show diferent task per day

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  include TasksHelper
   before_action :authenticate_and_set_user
   before_action :authenticate_and_set_user_or_admin
   skip_before_action :verify_authenticity_token, only: [:create]
@@ -8,12 +9,18 @@ class TasksController < ApplicationController
     render json: @tasks
   end
 
+  def show
+    set_lists
+    render json: @tasks
+  end
+
   def create
     @task = current_user.tasks.new(task_params)
-    @progress= get_progress (@task)
-    @task.progress = @progress.to_i
     @task.expiration_day = DateTime.now
-      if @task.save
+    if @task.save
+        @progress= get_progress (@task)
+        @task.progress = @progress.to_i
+        @task.save
         render_success(message: 'OK')
       else
         render json: @task.errors, status: :unprocessable_entity
@@ -22,9 +29,9 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
-    @task.progress= get_progress(@task)
     @task.expiration_day = DateTime.now
-      if @task.save
+    if @task.save
+        @task.progress= get_progress(@task).save
         render_success(message: 'Task updated!')
       else
         render json: @task.errors, status: :unprocessable_entity
@@ -35,21 +42,9 @@ class TasksController < ApplicationController
   #Date.today-1
   #scope :range, ->() { where("created_at < ? AND "created_at >?, Date.today-7, Date.today)}
 
-  def get_progress (object)
-    @time = object.time
-    @goal = object.goal
-    @progress = @time*100/@goal
-    return @time > @goal ? (@progress-100)*(-1) : @progress
-  end
-
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
   end
 
-  private
-
-  def task_params
-    params.require(:task).permit(:name, :time, :goal, :progress, :expiration_day, :img)
-  end
 end
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,31 @@
 module TasksHelper
+
+
+  def set_lists
+    case params[:id]
+    when 'yesterday'
+      @tasks = current_user.tasks.yesterday
+
+    when 'week'
+      @tasks = current_user.tasks.per_week
+
+    else
+      @tasks = current_user.tasks.today
+    end
+  end
+
+
+  def get_progress (object)
+    @time = object.time
+    @goal = object.goal
+    @progress = @time*100/@goal
+    return @time > @goal ? (@progress-100)*(-1) : @progress
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :time, :goal, :progress, :expiration_day, :img)
+  end
+
 end
