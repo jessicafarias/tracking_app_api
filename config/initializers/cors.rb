31637db5a99d571e833dcae89a1dccff11755c25
jessica.farias.rosado@gmail.com <jessica.farias.rosado@gmail.com>
2021-03-18 17:01:33 -0600

    docs:Allow cors to frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,10 +2,20 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3000'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      credentials: true
+  end
+
+  allow do
+    origins 'https://vigorous-minsky-e3bf16.netlify.app/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      credentials: true
   end
 end
