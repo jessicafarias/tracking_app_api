d64d26cc176129ab8222b05535d34ed5eaffde87
jessica.farias.rosado@gmail.com <jessica.farias.rosado@gmail.com>
2021-03-22 13:33:35 -0600

    fix: Adding format validation to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   has_many :tasks, class_name: 'Task', foreign_key: 'user_id'
   has_secure_password
-  # has_secure_password :recovery_password, validations: false
 end
